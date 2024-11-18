307aa4757a85480fc90d21dec8e094335e0c0870
Mason <masonfilbrandt@gmail.com>
2024-11-18 12:02:51 -0500

    v0.1

---
+++ Assets/Scripts/Spongy.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spongy : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3.0f;
+    [SerializeField]
+    private GUIStyle guiStyle;
+
+    public int score = 0;
+
+    private int lives = 3;
+    private int timerMinutes = 0;
+    private int timerSeconds = 0;
+    private float sideLimit;
+
+    private Renderer spongyRenderer;
+    private float spongyHalfWidth;
+
+    private GameObject backgroundObject;
+    private Renderer backgroundRenderer;
+    private GameObject[] gameObjects;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spongyRenderer = GetComponent<Renderer>();
+        spongyHalfWidth = spongyRenderer.bounds.size.x / 2;
+
+        backgroundObject = GameObject.Find("Bathroom");
+        backgroundRenderer = backgroundObject.GetComponent<Renderer>();
+
+        sideLimit = backgroundRenderer.bounds.size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        // Moving Left
+        if(Input.GetKey(KeyCode.A))
+        {
+            if(transform.position.x > -sideLimit + spongyHalfWidth)
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+        }
+
+        // Moving Right
+        if(Input.GetKey(KeyCode.D))
+        {
+            if(transform.position.x < sideLimit - spongyHalfWidth)
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name.Equals("Acid(Clone)"))
+        {
+            lives--;
+            if(lives <= 0)
+            {
+                RemovalGrime();
+                RemovalAcid();
+                Time.timeScale = 0;
+            }
+        }
+
+        if(collision.gameObject.name.Equals("Grime(Clone)"))
+        {
+            Controller.score += 50;
+        }
+    }
+
+    private void OnGUI()
+    {
+        timerMinutes = (int) Time.time / 60;
+        timerSeconds = (int) Time.time % 60;
+
+        GUI.Box(new Rect(10, 10, 100, 30), "Time: " + timerMinutes + ":" + timerSeconds, guiStyle);
+        GUI.Box(new Rect(400, 10, 100, 30), "Score: " + Controller.score, guiStyle);
+        GUI.Box(new Rect(400, 45, 100, 30), "Lives: " + lives, guiStyle);
+    }
+
+    private void RemovalGrime()
+    {
+        gameObjects = GameObject.FindGameObjectsWithTag("Grime");
+        foreach(GameObject grimeObject in gameObjects) Destroy(grimeObject);
+    }
+
+    private void RemovalAcid()
+    {
+        gameObjects = GameObject.FindGameObjectsWithTag("Acid");
+        foreach(GameObject acidObject in gameObjects) Destroy(acidObject);
+    }
+}
